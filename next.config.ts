869adde04b64a7d6ce4f3d8f9682b5684d8b23d7
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2025-07-15 16:44:12 +0530

    image domain added

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["pexels.com"],
+    domains: ["images.pexels.com", "img.icons8.com"],
   },
 };
 
